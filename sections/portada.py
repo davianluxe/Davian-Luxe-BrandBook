bc54da7627b9d2ebdbc4ab9a7170ea0f7afa6529
davianluxe <davianluxesas@gmail.com>
2026-07-22 13:32:22 -0500

    Add portada.py to create PDF cover page

---
+++ sections/portada.py
@@ -0,0 +1,6 @@
+from styles import titulo, subtitulo, parrafo
+
+def crear_portada(pdf):
+    titulo(pdf, "DAVIAN LUXE", 760)
+    subtitulo(pdf, "Brand Book", 725)
+    parrafo(pdf, "Premium Living • Exclusive Life", 690)
